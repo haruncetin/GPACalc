fd5de619e5e64346c0b64348290d90309c895df3
Harun ÇETİN <hcetin01@hotmail.com>
2019-02-13 11:26:49 +0300

    First commit

---
+++ GPACalc/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace GPACalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblTotalCredit = new System.Windows.Forms.Label();
+            this.lblGPA = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(248, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(26, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 34);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(230, 245);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Grade";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Credit";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(136, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "GPA";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(248, 63);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(26, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblTotalCredit
+            // 
+            this.lblTotalCredit.AutoSize = true;
+            this.lblTotalCredit.Location = new System.Drawing.Point(12, 293);
+            this.lblTotalCredit.Name = "lblTotalCredit";
+            this.lblTotalCredit.Size = new System.Drawing.Size(61, 13);
+            this.lblTotalCredit.TabIndex = 6;
+            this.lblTotalCredit.Text = "Total Credit";
+            // 
+            // lblGPA
+            // 
+            this.lblGPA.AutoSize = true;
+            this.lblGPA.Location = new System.Drawing.Point(132, 293);
+            this.lblGPA.Name = "lblGPA";
+            this.lblGPA.Size = new System.Drawing.Size(36, 13);
+            this.lblGPA.TabIndex = 7;
+            this.lblGPA.Text = "CGPA";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(248, 102);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(26, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "=";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(248, 256);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(24, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "?";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 320);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.lblGPA);
+            this.Controls.Add(this.lblTotalCredit);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GPA Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblTotalCredit;
+        private System.Windows.Forms.Label lblGPA;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ GPACalc/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace GPACalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ComboBox cbGrade = new ComboBox();
+            cbGrade.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbGrade.Items.Add("AA");
+            cbGrade.Items.Add("BA");
+            cbGrade.Items.Add("BB");
+            cbGrade.Items.Add("CB");
+            cbGrade.Items.Add("CC");
+            cbGrade.Items.Add("DC");
+            cbGrade.Items.Add("DD");
+            cbGrade.Items.Add("FD");
+            cbGrade.Items.Add("FF");
+            cbGrade.Width = 50;
+            cbGrade.SelectedIndex = 0;
+            flowLayoutPanel1.Controls.Add(cbGrade);
+            TextBox txtCredit = new TextBox();
+            txtCredit.Width = 60;
+            txtCredit.TextAlign = HorizontalAlignment.Right;
+            flowLayoutPanel1.Controls.Add(txtCredit);
+            TextBox txtGPA = new TextBox();
+            txtGPA.Width = 80;
+            txtGPA.ReadOnly = true;
+            txtGPA.TextAlign = HorizontalAlignment.Right;
+            flowLayoutPanel1.Controls.Add(txtGPA);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ComboBox cbGrade = new ComboBox();
+            cbGrade.Items.Add("AA");
+            cbGrade.Items.Add("BA");
+            cbGrade.Items.Add("BB");
+            cbGrade.Items.Add("CB");
+            cbGrade.Items.Add("CC");
+            cbGrade.Items.Add("DC");
+            cbGrade.Items.Add("DD");
+            cbGrade.Items.Add("FD");
+            cbGrade.Items.Add("FF");
+            cbGrade.Width = 50;
+            cbGrade.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbGrade.SelectedIndex = 0;
+            flowLayoutPanel1.Controls.Add(cbGrade);
+
+            TextBox txtCredit = new TextBox();
+            txtCredit.Width = 60;
+            txtCredit.TextAlign = HorizontalAlignment.Right;
+            flowLayoutPanel1.Controls.Add(txtCredit);
+            
+            TextBox txtGPA = new TextBox();
+            txtGPA.Width = 80;
+            txtGPA.ReadOnly = true;
+            txtGPA.TextAlign = HorizontalAlignment.Right;
+            flowLayoutPanel1.Controls.Add(txtGPA);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (flowLayoutPanel1.Controls.Count > 3)
+            {
+                flowLayoutPanel1.Controls.RemoveAt(flowLayoutPanel1.Controls.Count - 1);
+                flowLayoutPanel1.Controls.RemoveAt(flowLayoutPanel1.Controls.Count - 1);
+                flowLayoutPanel1.Controls.RemoveAt(flowLayoutPanel1.Controls.Count - 1);                
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double grade = 0.0;
+            double credit = 0.0;
+            double totalCredit = 0.0;
+            double gpa = 0.0;
+            double totalGpa = 0.0;
+
+            double cgpa = 0.0;
+
+            for (int i = 0; i < flowLayoutPanel1.Controls.Count; i+=3)
+            {
+                switch ((flowLayoutPanel1.Controls[i] as Control).Text)
+	            {
+                    case "AA":
+                        grade = 4.0;
+                    break;
+                    case "BA":
+                        grade = 3.5;
+                    break;
+                    case "BB":
+                        grade = 3.0;
+                    break;
+                    case "CB":
+                        grade = 2.5;
+                    break;
+                    case "CC":
+                        grade = 2.0;
+                    break;
+                    case "DC":
+                        grade = 1.5;
+                    break;
+                    case "DD":
+                        grade = 1.0;
+                    break;
+                    case "FD":
+                        grade = 0.5;
+                    break;
+                    case "FF":
+                        grade = 0.0;
+                    break;
+                }
+                credit = Convert.ToDouble((flowLayoutPanel1.Controls[i + 1] as Control).Text == "" ? "0" : (flowLayoutPanel1.Controls[i + 1] as Control).Text);
+                (flowLayoutPanel1.Controls[i + 2] as Control).Text = (grade * credit).ToString();
+                gpa = grade * credit;
+                totalCredit += credit;
+                totalGpa += gpa;
+            }
+            cgpa = Math.Round(Convert.ToDouble(totalGpa/totalCredit),2);
+            lblTotalCredit.Text = "Total Credit: " + totalCredit.ToString();
+            lblGPA.Text = "CGPA: " + cgpa.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this,"Author Harun CETIN (c) 2019 (hcetin01@hotmail.com)","About");
+        }
+    }
+}
